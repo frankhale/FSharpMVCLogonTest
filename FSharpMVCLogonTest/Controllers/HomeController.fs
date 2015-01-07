73e8dfbfdd3fb0d82253b6c9eca871a9b50f438e
Frank Hale <frankhale@gmail.com>
2015-01-06 21:45:52 -0500

    Run CodeMaid on the controller

--- FSharpMVCLogonTest/Controllers/HomeController.fs
+++ FSharpMVCLogonTest/Controllers/HomeController.fs
@@ -8,46 +8,43 @@ open System.Web
 open System.Web.Mvc
 open System.Web.Mvc.Ajax
 open System.Web.Routing
-
 open FSharpMVCLogonTest.Infrastructure
 open FSharpMVCLogonTest.Models
 
-[<HandleError>] 
+[<HandleError>]
 type HomeController() =
-    inherit Controller()
+  inherit Controller()
 
-    let ValidateUser(model : UsernamePasswordModel) =
-      // Just do something simple so we can see if it works, LOL!
-      match model.UserName.ToLower(), model.Password with
-      | "frank", "123456" -> true
-      | "matt", "123456" -> true
-      | _ -> false
+  let ValidateUser(model : UsernamePasswordModel) =
+    // Just do something simple so we can see if it works, LOL!
+    match model.UserName.ToLower(), model.Password with
+    | "frank", "123456" -> true
+    | "matt", "123456" -> true
+    | _ -> false
 
-    member private this.InvalidLoginAttempt (model : UsernamePasswordModel) = 
-      this.ModelState.AddModelError("", "Invalid login attempt.")
-      this.View(model) :> ActionResult
-    
-    member this.Index () = this.View()
-    
-    member this.Logon () = this.View()    
-    
-    [<HttpPost>]
-    [<ValidateAntiForgeryToken>]
-    member this.Logon (model : UsernamePasswordModel) =          
-      match this.ModelState.IsValid with
+  member private this.InvalidLoginAttempt(model : UsernamePasswordModel) =
+    this.ModelState.AddModelError("", "Invalid login attempt.")
+    this.View(model) :> ActionResult
+
+  member this.Index() = this.View()
+  member this.Logon() = this.View()
+
+  [<HttpPost>]
+  [<ValidateAntiForgeryToken>]
+  member this.Logon(model : UsernamePasswordModel) =
+    match this.ModelState.IsValid with
+    | false -> this.InvalidLoginAttempt(model)
+    | true ->
+      match ValidateUser(model) with
+      | true ->
+        this.Session.["CurrentUser"] <- model.UserName
+        this.RedirectToAction("Index", "Home") :> ActionResult
       | false -> this.InvalidLoginAttempt(model)
-      | true ->  
-        match ValidateUser(model) with
-        | true -> 
-            this.Session.["CurrentUser"] <- model.UserName
-            this.RedirectToAction("Index", "Home") :> ActionResult
-        | false -> this.InvalidLoginAttempt(model)
-      
-    [<AuthorizeActionFilter>]
-    member this.Logout () = 
-      this.Session.["CurrentUser"] <- null
-      this.RedirectToAction("Index", "Home")
 
-    [<AuthorizeActionFilter>]
-    member this.Secret () =
-      this.View()
+  [<AuthorizeActionFilter>]
+  member this.Logout() =
+    this.Session.["CurrentUser"] <- null
+    this.RedirectToAction("Index", "Home")
+
+  [<AuthorizeActionFilter>]
+  member this.Secret() = this.View()
