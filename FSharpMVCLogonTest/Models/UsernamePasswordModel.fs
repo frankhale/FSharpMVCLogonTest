fb161b337d92ba05f652eaa112aec6c60a6a0807
Frank Hale <frankhale@gmail.com>
2015-01-07 23:53:36 -0500

    Make the UsernamePasswordModel better!

--- FSharpMVCLogonTest/Models/UsernamePasswordModel.fs
+++ FSharpMVCLogonTest/Models/UsernamePasswordModel.fs
@@ -1,16 +1,13 @@
 namespace FSharpMVCLogonTest.Models
 
 open System.ComponentModel.DataAnnotations
-
-type UsernamePasswordModel(un : string, pw : string) =
-
+  
+type UsernamePasswordModel() =
   [<Required>]
   [<StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 4)>]
-  member val UserName = un with get, set
+  member val UserName = Unchecked.defaultof<string> with get, set
 
   [<Required>]
   [<DataType(DataType.Password)>]
   [<StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)>]
-  member val Password = pw with get, set
-
-  new() = UsernamePasswordModel("", "")
+  member val Password = Unchecked.defaultof<string> with get, set
